4f05a41ab8b9c313149a2d2c3e9e867b2b7163bb
Bruno Esposito <bacaesposito97@gmail.com>
2024-02-27 23:56:40 -0300

    exarraysjs

---
+++ ArraysJS2/arrays2.js
@@ -0,0 +1,22 @@
+// let para criar a variavel 'titulo'
+let titulo = document.querySelector('h1'); //document.querySelect('h1') para conectar ao elemento h1 do HTML
+titulo.innerHTML = 'Hora do Desafio!';
+
+//criar a função verificarclick que esta no HTML no onclick
+function verificarClick(){
+    console.log('O botão foi clicado');
+};// a função criada vai aprensentar uma mensagem no console sempre que for clicado no boto consoler
+
+function mensagemAlert(){
+    alert('Eu amo JS');
+};
+function promptCidade(){
+    cidade = prompt('Qual o nome dessa cidade?');
+    alert(`Estive em ${cidade} e lembrei de você` );
+};
+function numeroSoma(){
+    numero1 = parseInt(prompt('Informe o primeiro número: '));
+    numero2 = parseInt(prompt('Informe o segundo número: '));
+    soma = (numero1 + numero2);
+    alert(`A soma dos números ${numero1} e ${numero2} é: ${soma}`);
+};
---
+++ arrays.js
@@ -0,0 +1,16 @@
+//let listaGenerica = [];
+
+//let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+//let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+//linguagensDeProgramacao.push('Java', 'Ruby','GoLang');
+//console.log(linguagensDeProgramacao);
+
+//let clientes = ['Rachel','Gilberto','Elma'];
+//console.log(clientes[0]);
+
+//let clientes = ['Rachel','Gilberto','Elma'];
+//console.log(clientes[1]);
+
+//let clientes = ['Rachel','Gilberto','Elma'];
+//console.log(clientes[2]);
